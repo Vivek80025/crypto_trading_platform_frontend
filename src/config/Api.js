3614af334dd02a1c3aa32214abcd72d88d0ded02
Vivek80025 <vjha052003@gmail.com>
2026-01-12 21:43:29 +0530

    modify in backend

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// export const API_URL = "http://localhost:5455" 
-export const API_URL = "https://viva-trading-backend.zeabur.app" 
+// export const API_URL = "http://localhost:5455"
+export const API_URL = "https://trading-project-backend.zeabur.app" 
 export const Api = axios.create({
   baseURL: API_URL,
   headers: {
